8addeb8dfb3cee61bdf985cecc7e8388e412baa0
ajmal <ajmalash72@gmail.com>
2021-01-09 14:31:34 +0530

    admin page added

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   if(req.session.admin){
-    res.render('admin/admin')
+    res.render('admin/admin',{admin:true})
   }else{
     res.redirect('/')
   }
